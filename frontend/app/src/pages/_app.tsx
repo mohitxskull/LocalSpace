5bb8a8e4c49a1d3943a5567ef6d6a8908f2b484d
mohitxskull <mohitxskull@gmail.com>
2025-09-23 03:10:07 +0530

    WIP

--- frontend/app/src/pages/_app.tsx
+++ frontend/app/src/pages/_app.tsx
@@ -2,7 +2,6 @@ import "@mantine/core/styles.css";
 import "@mantine/nprogress/styles.css";
 import "@mantine/notifications/styles.css";
 import "@/styles/global.css";
-import "@localspace/ui/theme.css";
 
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
@@ -13,8 +12,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import { setting } from "@localspace/ui/configs/setting";
-import { shadcnTheme } from "@localspace/ui/configs/shadcn_theme";
-import { shadcnCssVariableResolver } from "@localspace/ui/configs/shadcn_css_variable_resolver";
+import { theme } from "@localspace/ui/configs/theme";
 import { client, TuyauProvider } from "@/lib/tuyau";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
@@ -84,10 +82,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
       <QueryClientProvider client={queryClient}>
         <TuyauProvider client={client} queryClient={queryClient}>
-          <MantineProvider
-            theme={shadcnTheme}
-            cssVariablesResolver={shadcnCssVariableResolver}
-          >
+          <MantineProvider theme={theme}>
             <NuqsAdapter>
               <NavigationProgress />
               <Notifications />
